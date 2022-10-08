fd368a2ddb0de7e80db0f5955b508424536ea6bd
Ryan Miller <ryan_miller2@hotmail.com>
2022-10-08 20:58:55 +0400

    Add &apos to about text

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,25 +8,32 @@ const About = () => {
         <MDBCol md="3"></MDBCol>
         <MDBCol md="4">
           <div>
-            Hello! My name is Restimewa, you can call me Resti. I was born and
-            raised in Surabaya, Indonesia. The land of many cultures mixed
-            together! I have worked for a variety of comapanies over my career,
-            including Digital Printing, Advertising, creating Logos/Brochures,
-            and also Website Design. Furthermore, I have experience with Product
-            Labels, and countless other deliverables. As a passionate graphic
-            designer who loves fine art, I know what a good design looks like.
+            <p>
+              Hello! My name is Restimewa, you can call me Resti. I was born and
+              raised in Surabaya, Indonesia. The land of many cultures mixed
+              together! I have worked for a variety of comapanies over my
+              career, including Digital Printing, Advertising, creating
+              Logos/Brochures, and also Website Design. Furthermore, I have
+              experience with Product Labels, and countless other deliverables.
+              As a passionate graphic designer who loves fine art, I know what a
+              good design looks like.
+            </p>
           </div>
           <div className="mt-3">
-            My interest outside of graphic design and fine arts, includes
-            listening to music, writing poetry, riding my scooter, hiking and
-            visting many temples which my country has a lot of! I like to lean
-            about history and culture. Eager to work with any brands or
-            businesses.
+            <p>
+              My interest outside of graphic design and fine arts, includes
+              listening to music, writing poetry, riding my scooter, hiking and
+              visting many temples which my country has a lot of! I like to lean
+              about history and culture. Eager to work with any brands or
+              businesses.
+            </p>
           </div>
           <div className="mt-3">
-            Have a question or business proposal? Fill out the contact form as I 
-            would love to help with your brand's needs and find a way I can
-            help.
+            <p>
+              Have a question or business proposal? Fill out the contact form as
+              I would love to help with your brand&apos;s needs and find a way
+              I can help.
+            </p>
           </div>
         </MDBCol>
         <MDBCol md="4">
