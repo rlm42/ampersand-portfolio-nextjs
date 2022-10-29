30ef0198ef5ee94cae9eb310ea964d0d333fa582
Ryan Miller <ryan_miller2@hotmail.com>
2022-10-29 22:11:11 +0400

    Install react-boostrap, change navbar to use it

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -10,21 +10,28 @@
 //   MDBIcon,
 // } from "mdb-react-ui-kit";
 
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Link from "next/link";
+
+// import * as React from "react";
+// import AppBar from "@mui/material/AppBar";
+// import Box from "@mui/material/Box";
+// import Toolbar from "@mui/material/Toolbar";
+// import IconButton from "@mui/material/IconButton";
+// import Typography from "@mui/material/Typography";
+// import Menu from "@mui/material/Menu";
+// import MenuIcon from "@mui/icons-material/Menu";
+// import Container from "@mui/material/Container";
+// import Avatar from "@mui/material/Avatar";
+// import Button from "@mui/material/Button";
+// import Tooltip from "@mui/material/Tooltip";
+// import MenuItem from "@mui/material/MenuItem";
+// import AdbIcon from "@mui/icons-material/Adb";
+// import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 import Image from "next/image";
 import ampersandLogo from "../../public/images/ampersandlogo2.png";
@@ -112,79 +119,101 @@ const SideNav = () => {
     //   </MDBBtn>
     // </div>
 
-    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      <div className="container-fluid">
-        <img src={ampersandLogo} width="200px" />
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/about">
-                About Me
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdownMenuLink"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Portfolio
-              </a>
-              <ul
-                className="dropdown-menu"
-                aria-labelledby="navbarDropdownMenuLink"
-              >
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Logos
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Flyers
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Websites
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    T-Shirts
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/contact">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    // <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    //   <div className="container-fluid">
+    //     <img src={ampersandLogo} width="200px" />
+    //     <button
+    //       className="navbar-toggler"
+    //       type="button"
+    //       data-bs-toggle="collapse"
+    //       data-bs-target="#navbarNavDropdown"
+    //       aria-controls="navbarNavDropdown"
+    //       aria-expanded="false"
+    //       aria-label="Toggle navigation"
+    //     >
+    //       <span className="navbar-toggler-icon"></span>
+    //     </button>
+    //     <div className="collapse navbar-collapse" id="navbarNavDropdown">
+    //       <ul className="navbar-nav">
+    //         <li className="nav-item">
+    //           <a className="nav-link active" aria-current="page" href="/">
+    //             Home
+    //           </a>
+    //         </li>
+    //         <li className="nav-item">
+    //           <a className="nav-link" href="/about">
+    //             About Me
+    //           </a>
+    //         </li>
+    //         <li className="nav-item dropdown">
+    //           <a
+    //             className="nav-link dropdown-toggle"
+    //             href="#"
+    //             id="navbarDropdownMenuLink"
+    //             role="button"
+    //             data-bs-toggle="dropdown"
+    //             aria-expanded="false"
+    //           >
+    //             Portfolio
+    //           </a>
+    //           <ul
+    //             className="dropdown-menu"
+    //             aria-labelledby="navbarDropdownMenuLink"
+    //           >
+    //             <li>
+    //               <a className="dropdown-item" href="#">
+    //                 Logos
+    //               </a>
+    //             </li>
+    //             <li>
+    //               <a className="dropdown-item" href="#">
+    //                 Flyers
+    //               </a>
+    //             </li>
+    //             <li>
+    //               <a className="dropdown-item" href="#">
+    //                 Websites
+    //               </a>
+    //             </li>
+    //             <li>
+    //               <a className="dropdown-item" href="#">
+    //                 T-Shirts
+    //               </a>
+    //             </li>
+    //           </ul>
+    //         </li>
+    //         <li className="nav-item">
+    //           <a className="nav-link" href="/contact">
+    //             Contact
+    //           </a>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //   </div>
+    // </nav>
 
+    <Navbar bg="light" expand="lg" className="fixed-top">
+      <Container>
+        <Navbar.Brand href="/">
+          {" "}
+          <img src={ampersandLogo} width="200px" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About Me</Nav.Link>
+            <NavDropdown title="Portfolio" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Logos</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Flyers</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Websits</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">T-Shirts</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     // </>
   );
 };
